aedf8f28c8292256b26f78f01bbf0bd47fb47791
Andrew Davis <andrewtdavis@hotmail.co.uk>
2020-06-26 02:22:29 +0100

    dropdown menu working

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 
+var newsType = "coronavirus" ;
+
+
+
+
+    // Select API feed by region and new and top 
+   document.getElementById("ulNode").addEventListener("click", function(e){
+    console.log(e.target);
+    let region = e.target.id;
+    search(region)
+    .then(results => {
+        console.log(results)
+        let output = '<div class="row">';
+        // Loop through
+        results.slice(2).forEach(post => {
+            // Check for image
+            const image = post.preview ? post.preview.images[0].source.url : 'https://cdn.pixabay.com/photo/2020/04/21/07/57/corona-5071972_960_720.jpg';
+            output += `
+            <div class="col-lg-6 col-md-6 mb-4">
+           <div class="card">
+            <img src="${image}" class="card-img-top" alt="..." style ="max-height: 285px;">
+            <div class="card-body">
+                <h6 class="card-title" style ="height: 96px;">${truncateText(post.title, 150)}</h6>
+               
+                <a href="${post.url}" target='_blank' class="btn btn-outline-secondary">Read More</a>
+            </div>
+            </div>
+            </div>
+           `;
+        })
+        output += '</div> ';
+        document.getElementById('results').innerHTML = output;
+    });
+    e.preventDefault();
+   });
+
+
 // API Fetch
- function search(searchTerm, SearchLimit, sortBy) {
-    return fetch('https://www.reddit.com/r/coronavirus/new.json?limit=100')
-    
+ function search(searchTerm = "coronavirus") {
+    return fetch(`https://www.reddit.com/r/${searchTerm}.json?limit=100`)
     .then(res => res.json())
     .then(data => data.data.children.map(data => data.data))
     .catch(err => console.log(err));
 };
 
  // Search reddit
- search()
+ search(newsType)
  .then(results => {
      console.log(results)
      let output = '<div class="row">';
@@ -41,3 +77,6 @@ function truncateText(text, limit) {
     if (shortened === -1) return text;
     return text.substring(0, shortened);
 };
+
+
+
